379698ce85826f81531c5a8dc50609bcfbf599e4
Vikalp Sharma <vikalpsh1234@gmail.com>
2024-01-17 08:44:46 +0530

    made call for help work

--- components/modals/CallForHelp.js
+++ components/modals/CallForHelp.js
@@ -1,37 +1,44 @@
-// CallForHelp.js
 import React, { useEffect, useState } from "react";
 import geoLocation from "../common/socialMedia/geolocation.js";
 import axios from "axios";
 import { ApiUrl } from "@/utils/BaseUrl";
-import { DataLayer } from "@/context/UserDataProvider";
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON, Popup, Marker } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+import userIcon from "../../public/assets/userloc.png";
+import { CiLocationOn } from "react-icons/ci";
 
 const CallForHelp = ({ visible, onClose = () => {}, callback = () => {}, data }) => {
   const [location, setLocation] = useState({
     loaded: false,
     coords: { lat: "", lng: "" },
   });
+  const [polLoc, setpolLoc] = useState();
   const [geojsonData, setGeojsonData] = useState(null);
   const locationData = geoLocation(setLocation);
 
   const gpsdata = async () => {
     try {
-      const response = await axios.post(`${ApiUrl}/api/calcDis`, {
-        userLat: location.coords.lat,
-        userLng: location.coords.lng
-      }, {
-        withCredentials: true
-      });
+      const response = await axios.post(
+        `${ApiUrl}/api/calcDis`,
+        {
+          userLat: location.coords.lat,
+          userLng: location.coords.lng,
+        },
+        {
+          withCredentials: true,
+        }
+      );
       console.log("Response Data: ");
       console.log(response.data);
       setGeojsonData(response.data.distance);
+      setpolLoc(response.data.policeLoc);
     } catch (error) {
-      console.error('Error fetching GeoJSON data:', error);
+      console.error("Error fetching GeoJSON data:", error);
     }
-  }
+  };
 
   useEffect(() => {
-    if(locationData.loaded){
+    if (locationData.loaded) {
       console.log(locationData.coords);
       gpsdata();
     }
@@ -48,6 +55,14 @@ const CallForHelp = ({ visible, onClose = () => {}, callback = () => {}, data })
 
   if (!visible) return null;
 
+  // User marker icon
+  const userMarkerIcon = new L.Icon({
+    iconUrl: "https://discord.com/channels/895681070513680396/920889077140697139/1197015123626295347",
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+  });
+
   return (
     <div
       id="background"
@@ -57,21 +72,33 @@ const CallForHelp = ({ visible, onClose = () => {}, callback = () => {}, data })
       }}
     >
       <div className="flex w-[60rem] h-[45rem] px-[2rem] py-[2rem] register-fir-bg">
-      <MapContainer
-        center={[location.coords.lat, location.coords.lng]}  // Set the initial center of the map
-        zoom={13}  // Set the initial zoom level of the map
-        style={{ height: '800px', width: '100%' }}
-        scrollWheelZoom={true}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"  
-        />
+        <MapContainer
+          center={[location.coords.lat, location.coords.lng]}
+          zoom={13}
+          scrollWheelZoom={true}
+          style={{ height: "650px", width: "100%" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
 
-        {geojsonData && (
-          <GeoJSON data={geojsonData} />
-        )}
-      </MapContainer>
+          {/* User Marker */}
+          {locationData.loaded && (
+            <Marker icon={userMarkerIcon} position={[location.coords.lat, location.coords.lng]}>
+              <Popup>User Location</Popup>
+            </Marker>
+          )}
+
+          {/* Police Marker */}
+          {polLoc && (
+            <Marker icon={userMarkerIcon} position={[polLoc.lat, polLoc.lng]}>
+              <Popup>Police Location</Popup>
+            </Marker>
+          )}
+
+          {geojsonData && <GeoJSON data={geojsonData} />}
+        </MapContainer>
       </div>
     </div>
   );
