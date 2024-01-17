87cc550d832d07207340b3eb741f5836202ad28f
Abhay1205Mishra <abhaym1205@gmail.com>
2024-01-17 22:27:10 +0530

    feedback changes

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preload" href="/portfoliosmall.jpg" as="image" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <meta name="theme-color" content="#181818" />
+        <link rel="icon" href="/images/icons/icon-maskable.png" sizes="192x192" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/user/feedback.js
+++ pages/user/feedback.js
@@ -1,10 +1,148 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import UserLayout from '../layout/UserLayout'
+import UserPanel from '@/components/common/Leftpanel.js/UserPanel'
+import SortData from '@/components/common/filter/SortData'
+import { BiSearch } from "react-icons/bi";
+import UserFirResgiterButton from '@/components/user/fir/UserFirResgiterButton'
+import { useContext } from 'react';
+import { ApiUrl } from '@/utils/BaseUrl';
+import axios from 'axios';
+import { DataLayer } from '@/context/UserDataProvider';
+import AllFeedback from '@/components/police/feedback/AllFeedback';
 
 const Feedback = () => {
-  return (
-    <div>
+  const [feedbacks, setFeedbacks] = useState([]);
+  const { refresh } = useContext(DataLayer);
 
-    </div>
+  const getFeedback = async () => {
+    if (sortValue == "Lastest") {
+      try {
+        const feedbackData = await axios.get(`${ApiUrl}/api/getFeedback`, {
+          withCredentials: true,
+        });
+        console.log("feedbackData");
+        console.log(feedbackData);
+        const mappedFeedbacks = feedbackData.data.feedbacks.map((feedback) => {
+          const reportDate = new Date(feedback.createdAt);
+          const daysOfWeek = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+          ];
+          const day = daysOfWeek[reportDate.getDay()];
+          const dayDate = reportDate.getDate();
+          const month = reportDate.getMonth() + 1;
+          const year = reportDate.getFullYear() % 100;
+
+          // Format the date as "DD.MM.YY"
+          const formattedDate = `${dayDate}.${month}.${year}`;
+
+          return {
+            name: feedback.userName,
+            age: feedback.userAge,
+            report: feedback.userFeedback,
+            hero: feedback.hero,
+            reportedDate: formattedDate,
+            reportedDay: day,
+            rating: feedback.rating,
+          };
+        });
+        setFeedbacks(mappedFeedbacks);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      try {
+        const feedbackData = await axios.get(`${ApiUrl}/api/getFeedbackOld`, {
+          withCredentials: true,
+        });
+        console.log("feedbackData");
+        console.log(feedbackData);
+        const mappedFeedbacks = feedbackData.data.feedbacks.map((feedback) => {
+          const reportDate = new Date(feedback.createdAt);
+          const daysOfWeek = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+          ];
+          const day = daysOfWeek[reportDate.getDay()];
+          const dayDate = reportDate.getDate();
+          const month = reportDate.getMonth() + 1;
+          const year = reportDate.getFullYear() % 100;
+
+          // Format the date as "DD.MM.YY"
+          const formattedDate = `${dayDate}.${month}.${year}`;
+
+          return {
+            name: feedback.userName,
+            age: feedback.userAge,
+            report: feedback.userFeedback,
+            hero: feedback.hero,
+            reportedDate: formattedDate,
+            reportedDay: day,
+            rating: feedback.rating,
+          };
+        });
+        setFeedbacks(mappedFeedbacks);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getFeedback();
+  }, [refresh]);
+
+  const [query, setQuery] = useState('');
+  const sortOptionData = [
+    {
+      id: 1,
+      option: "Latest",
+    },
+    {
+      id: 2,
+      option: "Oldest",
+    },
+  ]
+
+  const [sortValue, setSortValue] = useState(sortOptionData[0].option)
+  return (
+    <>
+      <UserLayout>
+        <div className=' bg-[#080F25] w-[100%] h-[100vh] flex flex-row relative ' >
+          <UserPanel />
+          <div className=' w-[81%] h-[100vh] flex flex-col items-center text-[#AEB9E1] pt-[1rem] px-[1.5rem] ' >
+
+            <div className=' w-[100%] flex justify-between items-center mt-[1rem] ' >
+              <SortData sortValue={sortValue} setSortValue={setSortValue} sortOptionData={sortOptionData} />
+              <div className=' ml-[-4rem] ' >
+                <UserFirResgiterButton />
+              </div>
+              <div className=' flex justify-between items-center bg-[#101935] h-[2.1rem] rounded-md border-[#191F35] border-[1px] text-[#7E88AE] px-[.8rem] ' >
+                <BiSearch className=' text-[1.1rem] ' />
+                <input onChange={(e) => setQuery(e.target.value)} type="text" className=' bg-[#101935] outline-none text-[.9rem] w-[87%] ' />
+              </div>
+            </div>
+
+            <div className=' w-[100%] mt-[2rem] ' >
+              <h2 className=' text-[#6C72FF] text-[1.1rem]  w-[15rem] mb-[.5rem] ' >Recent Feedback </h2>
+              <AllFeedback feedbackData={feedbacks} />
+            </div>
+
+          </div>
+        </div>
+      </UserLayout>
+
+    </>
   )
 }
 
