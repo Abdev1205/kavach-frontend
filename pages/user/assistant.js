2c626ee7a4d9e2b84375c991bae2fca5d39e9246
Vikalp Sharma <vikalpsh1234@gmail.com>
2024-01-18 09:41:44 +0530

    Added stages back

--- pages/user/assistant.js
+++ pages/user/assistant.js
@@ -25,13 +25,15 @@ const Assistant = () => {
         },
         { withCredentials: true }
       );
-
+      console.log(response);
       if (response.status === 200) {
         const responseData = response.data;
         setChatHistory((prevChat) => [
           ...prevChat,
-          { user: "bot", message: responseData.message },
+          { user: "Police bot", message: responseData.data },
         ]);
+        console.log("Chat history");
+        console.log(chatHistory);
       } else {
         console.error("Error sending message to backend");
       }
@@ -47,18 +49,25 @@ const Assistant = () => {
       <UserPanel />
       <div className=" flex flex-col items-center w-full">
         <div className="flex flex-col w-[50rem] items-center justify-end py-6 h-[100vh] border-2 border-borderBg">
-          <div className=" text-white flex-col flex-grow w-full px-4">
+          <span className=" text-2xl text-white">Police Bot</span>
+          <div className=" mt-8 w-full border-2 border-borderBg" />
+          <div className=" text-white flex-col flex-grow w-full px-4 overflow-y-auto">
             {chatHistory &&
-              chatHistory.map((item) => (
-                <div key={item} className=" bg-slate-800 rounded-xl h-20 p-2 my-4">
+              chatHistory.map((item, index) => (
+                <div
+                  key={`${item.user}-${item.message}-${index}`}
+                  className="bg-second rounded-xl h-26 p-2 my-4"
+                >
                   <div>{item.user}</div>
-                  <div className="">{item.message}</div>
+                  <div className="  w-full my-2 border-2 border-borderBg" />
+
+                  <div className="px-2">{item.message}</div>
                 </div>
               ))}
           </div>
           <div className="flex h-12">
             <input
-              className="bg-[#8C8C9A1F] text-[#bcc8f080] px-[1rem] py-[.5rem] rounded-md w-[40rem] outline-none focus:outline-[#6c71ff5c]"
+              className="bg-[#1F253B] text-[#bcc8f080] px-[1rem] py-[.5rem] rounded-md w-[40rem] outline-none focus:outline-[#6c71ff5c]"
               placeholder="Enter Query"
               required={true}
               value={userInput}
